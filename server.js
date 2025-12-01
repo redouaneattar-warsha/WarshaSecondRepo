87fdf09cb860fa34879994eab91f3039a6b7abbd
redouaneattar-warsha <redouane.attar.uae@gmail.com>
2025-12-01 16:51:42 +0400

    3rd commit , just to fix nodemon bug

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
+    if (req.url === '/') {res.end('Home Page');}
+    else if (req.url === '/about') {res.end('About Page');}
+    else {res.end('404 not found');}
+
+  
 });
 
 server.listen(4000, () => {
